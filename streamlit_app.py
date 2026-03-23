220303661a8e876705b9f3fef3120d8cd7f7e74b
veridelisi <72710290+veridelisi@users.noreply.github.com>
2026-03-23 09:58:40 +0000

    Fix nav

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from repl-nix-workspace!")
+import streamlit as st
 
+def main():
+    st.write("Hello from Modern Monetary System Game!")
 
 if __name__ == "__main__":
     main()
