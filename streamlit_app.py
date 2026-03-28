1f265f3d35ba8229848c18062281e5984f624b73
veridelisi <72710290+veridelisi@users.noreply.github.com>
2026-03-28 14:20:49 +0000

    update money game

--- streamlit_app.py
+++ streamlit_app.py
@@ -301,12 +301,12 @@ def build_flow(sc_id, amt):
         ]
     elif sc_id == 2:
         return [
-            {"id":"CentralBank","label":"Central Bank","abbr":"CB","bg":"#E1F5EE","border":"#1D9E75","color":"#0F6E56"},
-            {"arrow":True,"amt":f"{a} each","note":"reserves"},
-            {"id":"Xbank","label":"Bank X","abbr":"BX","bg":"#E6F1FB","border":"#378ADD","color":"#185FA5"},
-            {"arrow":True,"amt":a,"note":"reserves"},
-            {"id":"Ybank","label":"Bank Y","abbr":"BY","bg":"#EAF3DE","border":"#1D9E75","color":"#3B6D11"},
-        ]
+        {"id":"CentralBank","label":"Central Bank","abbr":"CB","bg":"#E1F5EE","border":"#1D9E75","color":"#0F6E56"},
+        {"arrow":True,"amt":f"{a} each","note":"reserves"},
+        {"id":"Xbank","label":"Bank X","abbr":"BX","bg":"#E6F1FB","border":"#378ADD","color":"#185FA5"},
+        {"arrow":True,"amt":"","note":""},   # <-- sadece bunlar boş
+        {"id":"Ybank","label":"Bank Y","abbr":"BY","bg":"#EAF3DE","border":"#1D9E75","color":"#3B6D11"},
+    ]
     elif sc_id == 3:
         return [
             {"id":"Ybank","label":"Bank Y","abbr":"BY","bg":"#EAF3DE","border":"#1D9E75","color":"#3B6D11"},
